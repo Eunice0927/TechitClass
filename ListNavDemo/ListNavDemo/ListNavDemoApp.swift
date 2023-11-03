c5e34dfcc1c099addc4edbd91c1b44358a389322
godlingto <godlingto@gmail.com>
2023-11-03 17:06:30 +0900

    SwiftUI List와 NavigationStack 튜토리얼: 프로젝트 준비

---
+++ ListNavDemo/ListNavDemo/ListNavDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListNavDemoApp.swift
+//  ListNavDemo
+//
+//  Created by 박준영 on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ListNavDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
