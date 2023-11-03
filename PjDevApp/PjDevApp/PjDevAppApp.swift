6bf68a2b50912cd49e29da23703b3e9647c4d26a
godlingto <godlingto@gmail.com>
2023-11-03 16:50:18 +0900

    DevPj first

---
+++ PjDevApp/PjDevApp/PjDevAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PjDevAppApp.swift
+//  PjDevApp
+//
+//  Created by 박준영 on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PjDevAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
