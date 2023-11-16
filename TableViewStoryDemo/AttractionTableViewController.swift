b60e66817f847f62b890195af59234e7cb76beaa
godlingto <godlingto@gmail.com>
2023-11-16 13:35:01 +0900

    테이블 뷰 데이터 소스 추가 - 앱이 로드될 때 일부 데이터로 배열을 초기화

--- TableViewStoryDemo/AttractionTableViewController.swift
+++ TableViewStoryDemo/AttractionTableViewController.swift
@@ -11,6 +11,9 @@ class AttractionTableViewController: UITableViewController {
     
     // 테이블 목록
     var items = ["Kim", "Lee", "Park"]
+    var attractionImages = [String]()
+    var attractionNames = [String]()
+    var webAddresses = [String]()
     
     @IBOutlet var tvListView: UITableView!
     
@@ -22,6 +25,34 @@ class AttractionTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        initialize()
+    }
+    
+    // 테이블 뷰 데이터 소스 생성
+    // 앱이 로드될 때 일부 데이터로 배열을 초기화
+    func initialize() {
+        attractionNames = ["Buckingham Palace",
+                           "The Eiffel Tower",
+                           "The Grand Canyon",
+                           "Windsor Castle",
+                           "Empire State Building"]
+        
+        webAddresses = ["https://en.wikipedia.org/wiki/Buckingham_Palace",
+                        "https://en.wikipedia.org/wiki/Eiffel_Tower",
+                        "https://en.wikipedia.org/wiki/Grand_Canyon",
+                        "https://en.wikipedia.org/wiki/Windsor_Castle",
+                        "https://en.wikipedia.org/wiki/Empire_State_Building"]
+        
+        attractionImages = ["buckingham_palace.jpg",
+                            "eiffel_tower.jpg",
+                            "grand_canyon.jpg",
+                            "windsor_castle.jpg",
+                            "empire_state_building.jpg"]
+        
+        // 테이블 뷰의 예상 행 높이 설정
+        // 테이블 보기 탐색을 추가할 때 행 높이가 축소되는 것을 방지하고, 테이블 렌더링 성능도 향상
+        self.tableView.estimatedRowHeight = 50
     }
     
     // 뷰가 보일 때 마다 리스트의 데이터를 다시 불러옴
