425d93320ab148e5a7028adb4f55e282fef56e66
godlingto <godlingto@gmail.com>
2023-11-21 11:30:01 +0900

    단어의 빈도수를 계산하고 저장하는 코드

--- FileExampleDemo/ViewController.swift
+++ FileExampleDemo/ViewController.swift
@@ -18,7 +18,41 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
 //        checkFile()
-        ex_04()
+        ex_05()
+    }
+    
+    func ex_05() {
+        let fileManager = FileManager.default
+        
+        do {
+            let documentPath = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0].path
+            let sourceFile = documentPath + "/" + "datafile.dat"
+            let destinationFile = documentPath + "/" + "frequency.txt"
+            
+            // 단어의 빈도수를 계산하고 저장하는 코드
+            let data = try Data(contentsOf: URL(fileURLWithPath: sourceFile))
+            if let text = String(data: data, encoding: .utf8) {
+                
+                let words = text.split(separator: " ").map { $0.lowercased() }
+                var frequency = [String:Int]()
+                for word in words {
+                    frequency[word, default: 0] += 1
+                }
+                var result = ""
+                for (word, count) in frequency {
+                    result += "\(word):\(count)\n"
+                }
+                
+                if let resultData = result.data(using: .utf8) {
+                    try resultData.write(to: URL(fileURLWithPath: destinationFile))
+                }
+            }
+            
+            // 변경된 파일 확인
+            
+        } catch {
+            print(error)
+        }
     }
     
     func ex_04() {
