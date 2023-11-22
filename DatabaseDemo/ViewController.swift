d24d44e7a54c813e0745e56f8eb2352cafb217a6
godlingto <godlingto@gmail.com>
2023-11-22 11:42:06 +0900

    데이터베이스 초기화

--- DatabaseDemo/ViewController.swift
+++ DatabaseDemo/ViewController.swift
@@ -14,9 +14,34 @@ class ViewController: UIViewController {
     @IBOutlet var phone: UITextField!
     @IBOutlet var status: UILabel!
     
+    var databasePath = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        initDB()
+    }
+    
+    // 데이터베이스 초기화
+    func initDB() {
+        let filemgr = FileManager.default
+        let dirPaths = filemgr.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        databasePath = dirPaths[0].appendingPathExtension("contacts.db").path
+        
+        if  !filemgr.fileExists(atPath: databasePath) {
+            
+            let contactDB = FMDatabase(path: databasePath)
+            if contactDB.open() {
+                let sql_stmt = "create table if not exists contacts ( id integer primary key autoincrement, name text, address text, phone text)"
+                if !contactDB.executeStatements(sql_stmt) {
+                    print("Error: \(contactDB.lastErrorMessage())")
+                }
+                contactDB.close()
+            } else {
+                print("Error: \(contactDB.lastErrorMessage())")
+            }
+        } // end if
     }
 
     @IBAction func saveContact(_ sender: Any) {
