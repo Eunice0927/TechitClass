f53f4058be7bc9c7ca8233e97e04288b654f4322
godlingto <godlingto@gmail.com>
2023-11-22 11:23:49 +0900

    사용자 인터페이스 디자인하기 - Name, Address, Phone, Label - Save, Find - @IBOutlet, @IBAction 연결

--- DatabaseDemo/ViewController.swift
+++ DatabaseDemo/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var name: UITextField!
+    @IBOutlet var address: UITextField!
+    @IBOutlet var phone: UITextField!
+    @IBOutlet var status: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func saveContact(_ sender: Any) {
+    }
+    
+    @IBAction func findContact(_ sender: Any) {
+    }
+    
 }
 
