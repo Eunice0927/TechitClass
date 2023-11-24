b00b0b13a386eb5193fd84729c3f176c11f6310b
godlingto <godlingto@gmail.com>
2023-11-24 14:11:41 +0900

    사용자 인터페이스 디자인 - 이미지 뷰, 도구 모음`toolbar` 및 두 개의 버튼 항목으로 구성 - @IBOutlet, @IBAction 연결

--- CameraDemo/ViewController.swift
+++ CameraDemo/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func useCamera(_ sender: Any) {
+    }
+    
+    @IBAction func useCameraRoll(_ sender: Any) {
+    }
+    
 }
 
