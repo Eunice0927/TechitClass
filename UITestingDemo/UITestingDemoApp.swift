64ccba989b0de21d256be7c37719e7b268ad4264
godlingto <godlingto@gmail.com>
2023-12-18 09:45:35 +0900

    Initial Commit

---
+++ UITestingDemo/UITestingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestingDemoApp.swift
+//  UITestingDemo
+//
+//  Created by 박준영 on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct UITestingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
