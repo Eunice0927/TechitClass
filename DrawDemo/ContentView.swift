152ca8e1ad8c3c80ba017e151cad7dbe32883c41
godlingto <godlingto@gmail.com>
2023-12-19 10:36:26 +0900

    커스텀 경로와 도형 그리기

--- DrawDemo/ContentView.swift
+++ DrawDemo/ContentView.swift
@@ -8,10 +8,42 @@ struct ContentView: View {
     var body: some View {
         ScrollView {
             VStack {
+                PathView()
+                Spacer().frame(height: 350)
+                MyShape()
+                    .fill(.red)
+                    .frame(width: 360, height: 350)
                 ShapeView()
             }
             .padding()
         }
+    }
+}
+
+struct MyShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+    
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addQuadCurve(to: CGPoint(x: rect.minX, y: rect.maxY),
+                          control: CGPoint(x: rect.midX, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.closeSubpath()
+        
+        return path
+    }
+}
+
+struct PathView: View {
+    var body: some View {
+        Path { path in
+            path.move(to: CGPoint(x: 10, y: 10))
+            path.addLine(to: CGPoint(x: 10, y: 350))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.closeSubpath()
+        }
+        .fill(.green)
     }
 }
 
