a70d0b0872dc48311fd43a99d0467b17ee810a01
godlingto <godlingto@gmail.com>
2023-12-19 10:05:23 +0900

    SwiftUI 기본 도형 그리기

--- DrawDemo/ContentView.swift
+++ DrawDemo/ContentView.swift
@@ -2,23 +2,62 @@
 //  ContentView.swift
 //  DrawDemo
 //
-//  Created by 박준영 on 12/19/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        ScrollView {
+            VStack {
+                ShapeView()
+            }
+            .padding()
+        }
+    }
+}
+
+struct ShapeView: View {
+    var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("기본 도형")
+            
+            Rectangle()
+            
+            Circle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
+            Capsule()
+//                .fill(.blue)
+                .stroke(lineWidth: 10)
+                .foregroundStyle(.blue)
+                .frame(width: 200, height: 100)
+            
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(style: StrokeStyle(lineWidth: 8, dash: [CGFloat(10), CGFloat(20)]))
+                .foregroundStyle(.blue)
+                .frame(width: 200, height: 100)
+            
+            Ellipse()
+                .stroke(style: StrokeStyle(lineWidth: 20, 
+                                           dash: [CGFloat(10), CGFloat(5), CGFloat(2)],
+                                          dashPhase: 10))
+                .foregroundStyle(.blue)
+                .frame(width: 250, height: 150)
+            
+            Text("테두리 오버레이 사용")
+            Ellipse()
+                .fill(.red)
+                .overlay {
+                    Ellipse()
+                        .stroke(.blue, lineWidth: 10)
+                }
+                .frame(width: 250, height: 150)
+            
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
-}
+//#Preview {
+//    ContentView()
+//}
