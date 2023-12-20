5cd677809a641bb78f6a5420a73d8aa3d3580b57
godlingto <godlingto@gmail.com>
2023-12-20 10:07:15 +0900

    탐색 메뉴 애니메이션 - 메뉴 항목이 선택될 때마다 뷰 변경에 애니메이션을 적용하려면 @Namespace 변수를 만들고 .matchedGeometryEffect() 수정자를 보라색 텍스트 보기에 연결

--- DrawDemo/AniNavigationMenuView.swift
+++ DrawDemo/AniNavigationMenuView.swift
@@ -16,6 +16,9 @@ struct AniNavigationMenuView: View {
 // 중복된 코드 ForEach 를 사용하여 단순화
 struct NavigationMenu: View {
     
+    // 메뉴 항목이 선택될 때마다 뷰 변경에 애니메이션을 적용하는 네임스페이스 변수
+    @Namespace private var menuItemTransition
+    
     @State var selectedIndex = 0
     let menuItems = ["Travel", "Nature", "Architecture"]
     
@@ -31,6 +34,9 @@ struct NavigationMenu: View {
                         .padding(.vertical, 4)
                         .background(Capsule().foregroundStyle(.purple))
                         .foregroundStyle(.white)
+                        // 메뉴 항목이 선택될 때마다 보기 변경에 애니메이션을 적용
+                        // ID 와 네임스페이스는 동일한 전환의 일부인 뷰를 식별하는데 사용
+                        .matchedGeometryEffect(id: "menuItem", in: menuItemTransition)
                 } else {
                     Text(menuItems[index])
                         .padding(.horizontal)
@@ -48,6 +54,8 @@ struct NavigationMenu: View {
         }
         .frame(minWidth: 0, maxWidth: .infinity)
         .padding()
+        // 뷰 애니메이션을 활성화하려고 .animation 수정자를 HStack 뷰에 연결
+        .animation(.easeInOut, value: selectedIndex)
     }
 }
 
