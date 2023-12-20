296609f3c859998da9704206b173e2e54c72bd37
godlingto <godlingto@gmail.com>
2023-12-20 09:46:29 +0900

    중복된 코드 ForEach 를 사용하여 단순화 - 선택한 메뉴 항목을 추적하기 위한 상태 변수 추가 (selectedIndex) - 메뉴 항목이 선택되면 해당 항목이 보라색으로 강조표시, 그렇지 않으면 배경색이 밝은 회색으로 설정 - 사용자의 터치를 감지하기 위해 .onTapGesture 수정자를 텍스트 뷰에 연결 - 탭하면 selectedIndex 값을 업데이트하여 선택한 텍스트 보기를 강조 표시

--- DrawDemo/AniNavigationMenuView.swift
+++ DrawDemo/AniNavigationMenuView.swift
@@ -8,37 +8,46 @@
 import SwiftUI
 
 struct AniNavigationMenuView: View {
+    var body: some View {
+        NavigationMenu()
+    }
+}
+
+// 중복된 코드 ForEach 를 사용하여 단순화
+struct NavigationMenu: View {
     
+    @State var selectedIndex = 0
     let menuItems = ["Travel", "Nature", "Architecture"]
     
     var body: some View {
         HStack {
             Spacer()
             
-            Text(menuItems[0])
-                .padding(.horizontal)
-                .padding(.vertical, 4)
-                .background(Capsule().foregroundStyle(.purple))
-                .foregroundStyle(.white)
+            ForEach(menuItems.indices, id: \.self) { index in
+                
+                if index == selectedIndex {
+                    Text(menuItems[index])
+                        .padding(.horizontal)
+                        .padding(.vertical, 4)
+                        .background(Capsule().foregroundStyle(.purple))
+                        .foregroundStyle(.white)
+                } else {
+                    Text(menuItems[index])
+                        .padding(.horizontal)
+                        .padding(.vertical, 4)
+                        .background(Capsule().foregroundStyle(Color(uiColor: UIColor.systemGray5)))
+                        .foregroundStyle(.black)
+                        .onTapGesture {
+                            selectedIndex = index
+                        }
+                }
+                
+                Spacer()
+            }
             
-            Spacer()
-            
-            Text(menuItems[1])
-                .padding(.horizontal)
-                .padding(.vertical, 4)
-                .background(Capsule().foregroundStyle(Color(uiColor: UIColor.systemGray5)))
-                .foregroundStyle(.black)
-            
-            Spacer()
-            
-            Text(menuItems[2])
-                .padding(.horizontal)
-                .padding(.vertical, 4)
-                .background(Capsule().foregroundStyle(Color(uiColor: UIColor.systemGray5)))
-                .foregroundStyle(.black)
-            
-            Spacer()
         }
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .padding()
     }
 }
 
