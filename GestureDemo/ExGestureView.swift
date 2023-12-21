bc86fb2374530c60fe50e9401f1a786af4028cf5
godlingto <godlingto@gmail.com>
2023-12-21 16:32:25 +0900

    드래그 동작 사용 - 이미지를 드래그하여 이동할 수 있도록 - 프로젝트를 실행하고 이미지를 드래그하면 드래그가 끝난 후에도 이미지가 그대로 유지

--- GestureDemo/ExGestureView.swift
+++ GestureDemo/ExGestureView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct ExGestureView: View {
     var body: some View {
-        AniLongPressGestureView()
+//        AniLongPressGestureView()
+        AniDragGestureView()
     }
 }
 
@@ -36,6 +37,35 @@ struct AniLongPressGestureView: View {
                         self.isPressed.toggle()
                     }
             )
+    }
+}
+
+// 드래그 동작 사용
+//  이미지를 드래그하여 이동할 수 있도록
+//  프로젝트를 실행하고 이미지를 드래그하면 드래그가 끝난 후에도 이미지가 그대로 유지
+struct AniDragGestureView: View {
+    
+    @GestureState private var offset: CGSize = .zero
+    @State private var position: CGSize = .zero
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "staroflife.circle")
+                .font(.system(size: 100))
+                .offset(x: position.width + offset.width, y: position.height + offset.height)
+                .animation(.easeInOut, value: position)
+                .foregroundStyle(.green)
+                .gesture(
+                    DragGesture()
+                        .updating($offset) { value, state, transaction in
+                            state = value.translation
+                        }
+                        .onEnded { value in
+                            self.position.width = value.translation.width
+                            self.position.height = value.translation.height
+                        }
+                )
+        }
     }
 }
 
