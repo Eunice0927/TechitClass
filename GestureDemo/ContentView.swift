d90d9b395dac4bc5312e270e1e9217406abe93c1
godlingto <godlingto@gmail.com>
2023-12-21 11:08:46 +0900

    제스처 인식기를 변수에 할당하고 수정자에서 그 변수를 참조

--- GestureDemo/ContentView.swift
+++ GestureDemo/ContentView.swift
@@ -14,13 +14,24 @@ struct ContentView: View {
 }
 
 struct BasicGestureView: View {
+    
+    let tap = TapGesture()
+        .onEnded { print("Tapped") }
+    
+    let doubleTap = TapGesture(count: 2)
+        .onEnded { print("Double Tapped") }
+    
+    // minimumDuration: 호출할 때 필요한 최소 시간(초)
+    // maximumDistance: 접촉점이 뷰 밖으로 이동할 수 있는 최대 거리
+    let longPress = LongPressGesture(minimumDuration: 2, maximumDistance: 25)
+        .onEnded { _ in print("LongPressGesture") }
+    
     var body: some View {
         VStack {
             Image(systemName: "hand.point.right.fill")
-                .gesture(
-                    TapGesture()
-                        .onEnded { print("Tapped") }
-                )
+                .gesture(tap)
+                .gesture(doubleTap)
+                .gesture(longPress)
         }
         .padding()
     }
