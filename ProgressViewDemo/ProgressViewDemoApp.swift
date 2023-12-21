fdc22445f94bab6230cf5dde29a9ace5011490e3
godlingto <godlingto@gmail.com>
2023-12-21 16:42:39 +0900

    Initial Commit

---
+++ ProgressViewDemo/ProgressViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewDemoApp.swift
+//  ProgressViewDemo
+//
+//  Created by 박준영 on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
