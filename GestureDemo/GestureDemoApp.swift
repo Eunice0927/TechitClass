738a0726722ce0cc82be22c4e9a6222ffdbb1f8b
godlingto <godlingto@gmail.com>
2023-12-21 10:34:12 +0900

    Initial Commit

---
+++ GestureDemo/GestureDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureDemoApp.swift
+//  GestureDemo
+//
+//  Created by 박준영 on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct GestureDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
