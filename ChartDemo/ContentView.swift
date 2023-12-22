4e61feef923e3a33b97b1e0c04e97f978193b7c0
godlingto <godlingto@gmail.com>
2023-12-22 14:06:36 +0900

    차트에 데이터 전달 - Char 뷰에 직접 데이터 전달과 ForEach를 이용한 데이터 전달

--- ChartDemo/ContentView.swift
+++ ChartDemo/ContentView.swift
@@ -12,13 +12,52 @@ struct ContentView: View {
     var body: some View {
         VStack {
 //            BasicAreaChartView()
-            BasicBarChartView()
+//            BasicBarChartView()
+            DataAreaChartView()
         }
         .padding()
         .frame(height: 300)
     }
 }
 
+// 차트에 데이터 전달
+struct MontylyTemp: Identifiable {
+    var id = UUID()
+    var month: String
+    var degree: Int
+}
+
+let tempData: [MontylyTemp] = [
+    MontylyTemp(month: "Jan", degree: 50),
+    MontylyTemp(month: "Feb", degree: 43),
+    MontylyTemp(month: "Mar", degree: 61)
+]
+
+struct DataAreaChartView: View {
+    var body: some View {
+        
+        Chart(tempData) { data in
+            AreaMark(
+                x: .value("Month", data.month),
+                y: .value("Temp", data.degree)
+            )
+        }
+        Spacer()
+        // ForEach 를 이용한 위와 동일한 코드
+        Chart {
+            ForEach(tempData) { data in
+                AreaMark(
+                    x: .value("Month", data.month),
+                    y: .value("Temp", data.degree)
+                )
+            }
+        }
+        
+    }
+}
+
+
+// 차트 기본 그리기
 struct BasicAreaChartView: View {
     var body: some View {
         Chart {
