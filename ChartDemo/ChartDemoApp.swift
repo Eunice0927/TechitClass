75b45757771332705158ffaea8d021e5fb0edb04
godlingto <godlingto@gmail.com>
2023-12-22 13:35:04 +0900

    Initial Commit

---
+++ ChartDemo/ChartDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartDemoApp.swift
+//  ChartDemo
+//
+//  Created by 박준영 on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ChartDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
