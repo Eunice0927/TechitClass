e1008b181dd57ef7af3849718fc816e4079157df
godlingto <godlingto@gmail.com>
2023-12-22 13:41:35 +0900

    영역 차트 기초

--- ChartDemo/ContentView.swift
+++ ChartDemo/ContentView.swift
@@ -6,19 +6,41 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            BasicAreaChartView()
         }
         .padding()
+        .frame(height: 300)
     }
 }
 
-#Preview {
-    ContentView()
+struct BasicAreaChartView: View {
+    var body: some View {
+        Chart {
+            AreaMark(
+                x: PlottableValue.value("Month", "Jun"),
+                y: PlottableValue.value("Temp", 50)
+            )
+            
+            // PlottableValue 생략 가능
+            AreaMark(
+                x: .value("Month", "Feb"),
+                y: .value("Temp", 43)
+            )
+            
+            AreaMark(
+                x: .value("Month", "Mar"),
+                y: .value("Temp", 61)
+            )
+            
+        }
+    }
 }
+
+//#Preview {
+//    ContentView()
+//}
