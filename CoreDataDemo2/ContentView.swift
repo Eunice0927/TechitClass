6b501dc12fb1ae973f8ec949e576191f315871a7
godlingto <godlingto@gmail.com>
2023-12-27 14:15:36 +0900

    검색 기능 추가하기

--- CoreDataDemo2/ContentView.swift
+++ CoreDataDemo2/ContentView.swift
@@ -38,6 +38,12 @@ struct ContentView: View {
                         addProduct()
                     }
                     Spacer()
+                    // ResultView에 name 상태 객체와 viewContext에 대한 참조를 전달하도록
+                    NavigationLink(destination:
+                                    ResultView(name: name, viewContext: viewContext)) {
+                        Text("Find")
+                    }
+                    Spacer()
                     Button("Clear") {
                         name = ""
                         quantity = ""
@@ -151,6 +157,42 @@ struct ContentView: View {
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
+        }
+    }
+}
+
+// 검색 결과를 표시하는 화면
+struct ResultView: View {
+    
+    var name: String
+    var viewContext: NSManagedObjectContext
+    // 제품의 검색 결과가 List에 표시될 상태 프로퍼티
+    @State var matches: [Product]?
+    
+    var body: some View {
+        VStack {
+            List {
+                ForEach(matches ?? []) { match in
+                    HStack {
+                        Text(match.name ?? "Not found")
+                        Spacer()
+                        Text(match.quantity ?? "Not found")
+                    }
+                }
+            }
+            .navigationTitle("Result")
+        }
+        .task {
+            // Product 엔티티에서 fetchRequest 인스턴스를 가져옴
+            let fetchRequest: NSFetchRequest<Product> = Product.fetchRequest()
+            
+            // Product 엔티티에서 검색 조건을 정의
+            // CONTAINS 키워드 : 검색에서 지정된 텍스트를 포함하는 모든 제품을 검색
+            fetchRequest.entity = Product.entity()
+            fetchRequest.predicate = NSPredicate(format: "name CONTAINS %@", name)
+            
+            // 검색 결과를 matches 상태 객체에 할당
+            matches = try? viewContext.fetch(fetchRequest)
         }
     }
 }
