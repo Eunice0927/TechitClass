c1b6a7c512a7f2fea136ab0d1b4266a9c87611cd
godlingto <godlingto@gmail.com>
2023-12-27 11:24:55 +0900

    UI 디자인 - 기존 화면 윗 부분에 - 두 개의 TextField 뷰 - 두 개의 Button - Product 목록이 표시될 List 뷰로 구성

--- CoreDataDemo2/ContentView.swift
+++ CoreDataDemo2/ContentView.swift
@@ -9,14 +9,58 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    
+    @State var name: String = ""
+    @State var quantity: String = ""
+    
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
+    
+    @FetchRequest(entity: Product.entity(), sortDescriptors: [])
+    private var products: FetchedResults<Product>
+    
 
     var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Product name", text: $name)
+                TextField("Product quantity", text: $quantity)
+                
+                HStack {
+                    Spacer()
+                    Button("Add") {
+                        
+                    }
+                    Spacer()
+                    Button("Clear") {
+                        name = ""
+                        quantity = ""
+                    }
+                    Spacer()
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                
+                List {
+                    ForEach(products) { product in
+                        HStack {
+                            Text(product.name ?? "Not found")
+                            Spacer()
+                            Text(product.quantity ?? "Not found")
+                        }
+                    }
+                }
+                .navigationTitle("Product Database")
+            }
+            .padding()
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
+        
+        
         NavigationView {
             List {
                 ForEach(items) { item in
