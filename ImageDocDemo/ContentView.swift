a2d06f0b0f9f70e11f55f3393b0174170805d5de
godlingto <godlingto@gmail.com>
2023-12-27 10:11:14 +0900

    이미지 필터링(세피아) 적용 - CoreImage 프레임워크를 사용

--- ImageDocDemo/ContentView.swift
+++ ImageDocDemo/ContentView.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    
     @Binding var document: ImageDocDemoDocument
+    @State private var ciFilter = CIFilter.sepiaTone()
+    
+    let context = CIContext()
 
     var body: some View {
         VStack {
@@ -18,7 +24,7 @@ struct ContentView: View {
                 .padding()
             
             Button {
-                
+                filterImage()
             } label: {
                 Text("Filter Image")
             }
@@ -26,7 +32,24 @@ struct ContentView: View {
 
         }
     }
+    
+    func filterImage() {
+        ciFilter.intensity = Float(1.0)
+        
+        // UIImage를 CIImage로 변환
+        let ciImage = CIImage(image: document.image)
+        
+        // 세피아 톤 필터를 적용
+        ciFilter.setValue(ciImage, forKey: kCIInputImageKey)
+        guard let outputImage = ciFilter.outputImage else { return }
+        
+        // 다시 UIImage로 변환
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            document.image = UIImage(cgImage: cgImage)
+        }
+    }
 }
+
 
 //#Preview {
 //    ContentView(document: .constant(ImageDocDemoDocument()))
