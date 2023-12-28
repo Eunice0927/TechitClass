3786a6bc8df81260fdfe5780efa3aa2de760165c
godlingto <godlingto@gmail.com>
2023-12-28 11:15:08 +0900

    Initial Commit

---
+++ WidgetDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WidgetDemo
+//
+//  Created by 박준영 on 12/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
