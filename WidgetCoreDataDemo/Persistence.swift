6c390b090c3d9d492658cdadcb0572d19206642e
godlingto <godlingto@gmail.com>
2023-12-28 14:45:39 +0900

    위젯에서 코어 데이터 연동 - WeatherWidgetExtension.entitlements 추가 - 프로젝트 Build Phases -> TARGETS > ***WidgetExtension 선택 -> Compiile Sources 속성그룹에 CoreData *.xcdatamodeld 과 Persistence.swift 를 포함 - App Groups 앱 그룹을 설정

--- WidgetCoreDataDemo/Persistence.swift
+++ WidgetCoreDataDemo/Persistence.swift
@@ -27,27 +27,27 @@ struct PersistenceController {
         }
         return result
     }()
+    
+    // PersistenceController 클래스에서 먼저 앱 그룹의 URL을 가져오는 속성을 추가
+    var containerURL: URL {
+        return FileManager.default.containerURL(
+            forSecurityApplicationGroupIdentifier: "group.oz.WidgetCoreData")!
+    }
 
     let container: NSPersistentContainer
 
+    // 그런 다음 영구 컨테이너의 저장소 설명에 URL을 설정
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "WidgetCoreDataDemo")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        } else {
+            let storeURL = containerURL.appendingPathComponent("WidgetCoreData.sqlite")
+            container.persistentStoreDescriptions.first!.url = storeURL
         }
+        
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
