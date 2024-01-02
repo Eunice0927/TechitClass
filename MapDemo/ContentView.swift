a4913ad141dff9f8394ac6bd1689f839e3092015
godlingto <godlingto@gmail.com>
2024-01-02 10:44:06 +0900

    Initial Commit

---
+++ MapDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapDemo
+//
+//  Created by 박준영 on 1/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
