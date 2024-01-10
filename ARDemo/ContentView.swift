670e811830d9459d02d44ed2875dbfb468ba1f79
godlingto <godlingto@gmail.com>
2024-01-10 14:51:26 +0900

    사용자 그림을 3D 개체에 텍스쳐로 적용

--- ARDemo/ContentView.swift
+++ ARDemo/ContentView.swift
@@ -39,6 +39,8 @@ struct ContentView : View {
 
 struct ARViewContainer: UIViewRepresentable {
     
+    @EnvironmentObject var viewModel: ViewModel
+    
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
@@ -81,12 +83,18 @@ struct ARViewContainer: UIViewRepresentable {
     func updateUIView(_ uiView: ARView, context: Context) {}
     
     func makeCoordinator() -> Coordinator {
-        Coordinator()
+        Coordinator(viewModel: viewModel)
     }
     
 }
 
 class Coordinator: NSObject {
+    let viewModel: ViewModel
+    
+    init(viewModel: ViewModel) {
+        self.viewModel = viewModel
+    }
+    
     weak var view: ARView?
     
     @objc func handleTap(_ recognizer: UITapGestureRecognizer) {
@@ -109,6 +117,9 @@ class Coordinator: NSObject {
             // 모델 객체에 대한 충돌 모양 생성
             modelEntity.generateCollisionShapes(recursive: true)
             
+            // 뷰 모델의 드로잉에서 생성된 텍스처를 모델 엔티티 재질에 적용
+            modelEntity.model?.materials = [createTexture(drawing: viewModel.drawing?.cgImage)]
+            
             // 모델 엔티티를 앵커 엔티티의 하위 추가
             anchorEntity.addChild(modelEntity)
             // AR 뷰 장면에 앵커 엔티티 추가
@@ -117,6 +128,21 @@ class Coordinator: NSObject {
             // 모델 엔티티에 대한 제스처 설정
             view.installGestures(.all, for: modelEntity)
         }
+    }
+    
+    func createTexture(drawing: CGImage?) -> SimpleMaterial {
+        if let drawing = drawing {
+            let texture = try! TextureResource.generate(from: drawing, options: .init(semantic: .normal))
+            var material = SimpleMaterial()
+            material.color = .init(texture: .init(texture))
+            material.roughness = .float(0)
+            material.metallic = .float(0)   // 텍스처를 사용하여 재료 색상을 설정
+            
+            return material
+        }
+        
+        // 드로잉이 nil이면 간단한 머터리얼 반환
+        return SimpleMaterial(color: .white, roughness: .float(0), isMetallic: false)
     }
 }
 
