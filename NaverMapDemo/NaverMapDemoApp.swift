50a0072c43b6c009368a25b7a97b7fe6d19ca9a2
godlingto <godlingto@gmail.com>
2024-01-16 15:23:33 +0900

    Initial Commit

---
+++ NaverMapDemo/NaverMapDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaverMapDemoApp.swift
+//  NaverMapDemo
+//
+//  Created by 박준영 on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct NaverMapDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
