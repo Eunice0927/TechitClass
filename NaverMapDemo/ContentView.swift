7793ba7a12b7372be6505494a7a466a54be5055c
godlingto <godlingto@gmail.com>
2024-01-17 14:28:23 +0900

    지도 위치 이동(서울-부산, 카메라 이동) - https://navermaps.github.io/ios-map-sdk/guide-ko/3-2.html - 애니메이션 시간 2초

--- NaverMapDemo/ContentView.swift
+++ NaverMapDemo/ContentView.swift
@@ -9,14 +9,23 @@ import SwiftUI
 import NMapsMap
 
 struct ContentView: View {
+    
+    @State var coord: (Double, Double) = (126.974375, 37.579081)
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            NaverMap()
+            NaverMap(coord: coord)
                 .ignoresSafeArea(.all, edges: .top)
+            HStack {
+                Button("서울") {
+                    // 경복궁 근처
+                    coord = (126.974375, 37.579081)
+                }
+                Button("부산") {
+                    // 해운대 근처
+                    coord = (129.153612, 35.157231)
+                }
+            }
         }
         .padding()
     }
@@ -24,7 +33,11 @@ struct ContentView: View {
 
 struct NaverMap: UIViewRepresentable {
     
+    var coord: (Double, Double)
+    
     func makeUIView(context: Context) -> NMFNaverMapView {
+        // NMFNaverMapView: 지도 객체 생성
+        // https://navermaps.github.io/ios-map-sdk/guide-ko/2-1.html
         let view = NMFNaverMapView()
         view.showZoomControls = false
         view.mapView.positionMode = .direction
@@ -33,8 +46,17 @@ struct NaverMap: UIViewRepresentable {
         return view
     }
     
-    func updateUIView(_ uiView: NMFNaverMapView, context: Context) {}
-    
+    func updateUIView(_ uiView: NMFNaverMapView, context: Context) {
+        // NMGLatLng: 하나의 위경도 좌표를 나타내는 클래스
+        // https://navermaps.github.io/ios-map-sdk/guide-ko/2-2.html
+        let coord = NMGLatLng(lat: coord.1, lng: coord.0)
+        // 카메라 이동
+        // https://navermaps.github.io/ios-map-sdk/guide-ko/3-2.html
+        let cameraUpdate = NMFCameraUpdate(scrollTo: coord)
+        cameraUpdate.animation = .fly
+        cameraUpdate.animationDuration = 2
+        uiView.mapView.moveCamera(cameraUpdate)
+    }
 }
 
 //#Preview {
