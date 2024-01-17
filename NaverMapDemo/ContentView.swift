668ae38cff1d65d477590c68cb9cf8d15d752344
godlingto <godlingto@gmail.com>
2024-01-17 17:44:27 +0900

    CoreLocation을 이용한 현재 위치를 지속적으로 가져와서 지도에 표시하기 - 마커관리를 위해 기존 네이버맵을 래핑한 NaverMap 구조체 코드를 코디네이터로 이동

--- NaverMapDemo/ContentView.swift
+++ NaverMapDemo/ContentView.swift
@@ -7,9 +7,11 @@
 
 import SwiftUI
 import NMapsMap
+import CoreLocation
 
 struct ContentView: View {
     
+    @StateObject var locationManager = LocationManager.shared
     @State var coord: MyCoord = MyCoord("경복궁 근처", 37.579081, 126.974375)
     
     var body: some View {
@@ -23,9 +25,24 @@ struct ContentView: View {
                 Button("부산") {
                     coord = MyCoord("해운대 근처", 35.157231, 129.153612)
                 }
+                Spacer()
+                Button("현재위치 가져오기 시작") {
+                    locationManager.startUpdatingLocation()
+                }
             }
         }
         .padding()
+        .onChange(of: locationManager.currentLocation) {
+            if let currentLocation = locationManager.currentLocation {
+                coord = MyCoord("현재위치",
+                                currentLocation.coordinate.latitude,
+                                currentLocation.coordinate.longitude)
+                print("현재위치: \(coord)")
+            }
+        }
+        .onDisappear {
+            locationManager.stopUpdatingLocation()
+        }
     }
 }
 
@@ -34,82 +51,93 @@ struct NaverMap: UIViewRepresentable {
     var coord: MyCoord
     
     func makeCoordinator() -> Coordinator {
-        Coordinator(coord)
+        Coordinator()
     }
     
     func makeUIView(context: Context) -> NMFNaverMapView {
-        // NMFNaverMapView: 지도 객체 생성
-        // https://navermaps.github.io/ios-map-sdk/guide-ko/2-1.html
-        let view = NMFNaverMapView()
-        view.showZoomControls = false
-        view.mapView.positionMode = .direction
-        view.mapView.zoomLevel = 17
-        
-        // 카메라 델리게이트 추가(이벤트 처리)
-        view.mapView.addCameraDelegate(delegate: context.coordinator)
-        
-        return view
+        context.coordinator.getNaverMapView()
     }
     
     func updateUIView(_ uiView: NMFNaverMapView, context: Context) {
-        // NMGLatLng: 하나의 위경도 좌표를 나타내는 클래스
-        // https://navermaps.github.io/ios-map-sdk/guide-ko/2-2.html
-        let coord = NMGLatLng(lat: coord.lat, lng: coord.lng)
-        
-        // 카메라 이동
-        // https://navermaps.github.io/ios-map-sdk/guide-ko/3-2.html
-        let cameraUpdate = NMFCameraUpdate(scrollTo: coord)
-        cameraUpdate.animation = .fly
-        cameraUpdate.animationDuration = 2
-        uiView.mapView.moveCamera(cameraUpdate)
-        
-        // 위치 오버레이 : 사용자의 현재 위치를 나타내는 데 특화된 오버레이
-        // https://navermaps.github.io/ios-map-sdk/guide-ko/5-1.html
-        // https://navermaps.github.io/ios-map-sdk/guide-ko/5-5.html
-        let locationOverlay = uiView.mapView.locationOverlay
-        locationOverlay.hidden = false
-        locationOverlay.location = coord
-
-        // 마커 : 지도상의 한 지점을 나타내기 위한 오버레이
-        // https://navermaps.github.io/ios-map-sdk/guide-ko/5-2.html
-        let marker = NMFMarker()
-        marker.position = coord
-        marker.captionText = self.coord.name
-        marker.mapView = uiView.mapView
-        
-        
-        // 정보 창 : 마커의 위 또는 지도의 특정 지점에 부가적인 정보를 나타내기 위한 오버레이
-        // https://navermaps.github.io/ios-map-sdk/guide-ko/5-3.html
-        // 정보 창을 마커와 연결
-        let infoWindow = NMFInfoWindow()
-        let dataSource = NMFInfoWindowDefaultTextSource.data()
-        dataSource.title = "정보 창 내용/마커를 탭하면 닫힘"
-        infoWindow.dataSource = dataSource
-        infoWindow.open(with: marker)
-        
-        // 마커 탭(클릭)했을 때 동작
-        marker.touchHandler = { (overlay: NMFOverlay) -> Bool in
-            print("Tap marker \(self.coord.name)")
-            // 정보 창과 상호작용
-            if let marker = overlay as? NMFMarker {
-                if marker.infoWindow == nil {
-                    // 현재 마커에 정보 창이 열려있지 않을 경우 엶
-                    infoWindow.open(with: marker)
-                } else {
-                    // 이미 현재 마커에 정보 창이 열려있을 경우 닫음
-                    infoWindow.close()
-                }
-            }
-            return true
-        }
+        context.coordinator.updateMapView(coord: coord)
     }
     
     class Coordinator: NSObject, NMFMapViewCameraDelegate {
         
-        var coord: MyCoord
+        var coord: MyCoord = MyCoord("", 0.0, 0.0)
         
-        init(_ coord: MyCoord) {
+        // NMFNaverMapView: 지도 객체 생성
+        // https://navermaps.github.io/ios-map-sdk/guide-ko/2-1.html
+        let view = NMFNaverMapView(frame: .zero)
+        
+        override init() {
+            super.init()
+
+            view.showZoomControls = false
+            view.mapView.positionMode = .direction
+            view.mapView.zoomLevel = 17
+            
+            // 카메라 델리게이트 추가(이벤트 처리)
+            view.mapView.addCameraDelegate(delegate: self)
+        }
+        
+        func getNaverMapView() -> NMFNaverMapView {
+            view
+        }
+        
+        func updateMapView(coord: MyCoord) {
             self.coord = coord
+            
+            // NMGLatLng: 하나의 위경도 좌표를 나타내는 클래스
+            // https://navermaps.github.io/ios-map-sdk/guide-ko/2-2.html
+            let coord = NMGLatLng(lat: coord.lat, lng: coord.lng)
+            
+            // 카메라 이동
+            // https://navermaps.github.io/ios-map-sdk/guide-ko/3-2.html
+            let cameraUpdate = NMFCameraUpdate(scrollTo: coord)
+            cameraUpdate.animation = .fly
+            cameraUpdate.animationDuration = 2
+            view.mapView.moveCamera(cameraUpdate)
+            
+            // 위치 오버레이 : 사용자의 현재 위치를 나타내는 데 특화된 오버레이
+            // https://navermaps.github.io/ios-map-sdk/guide-ko/5-1.html
+            // https://navermaps.github.io/ios-map-sdk/guide-ko/5-5.html
+            let locationOverlay = view.mapView.locationOverlay
+            locationOverlay.hidden = false
+            locationOverlay.location = coord
+
+            // 마커 : 지도상의 한 지점을 나타내기 위한 오버레이
+            // https://navermaps.github.io/ios-map-sdk/guide-ko/5-2.html
+            let marker = NMFMarker()
+            marker.position = coord
+            marker.captionText = self.coord.name
+            marker.mapView = view.mapView
+            
+            
+            // 정보 창 : 마커의 위 또는 지도의 특정 지점에 부가적인 정보를 나타내기 위한 오버레이
+            // https://navermaps.github.io/ios-map-sdk/guide-ko/5-3.html
+            // 정보 창을 마커와 연결
+            let infoWindow = NMFInfoWindow()
+            let dataSource = NMFInfoWindowDefaultTextSource.data()
+            dataSource.title = "정보 창 내용/마커를 탭하면 닫힘"
+            infoWindow.dataSource = dataSource
+            infoWindow.open(with: marker)
+            
+            // 마커 탭(클릭)했을 때 동작
+            marker.touchHandler = { (overlay: NMFOverlay) -> Bool in
+                print("Tap marker \(self.coord.name)")
+                // 정보 창과 상호작용
+                if let marker = overlay as? NMFMarker {
+                    if marker.infoWindow == nil {
+                        // 현재 마커에 정보 창이 열려있지 않을 경우 엶
+                        infoWindow.open(with: marker)
+                    } else {
+                        // 이미 현재 마커에 정보 창이 열려있을 경우 닫음
+                        infoWindow.close()
+                    }
+                }
+                return true
+            }
         }
         
         func mapView(_ mapView: NMFMapView, cameraWillChangeByReason reason: Int, animated: Bool) {
---
+++ NaverMapDemo/LocationManager.swift
@@ -0,0 +1,54 @@
+//
+//  LocationManager.swift
+//  NaverMapDemo
+//
+//  Created by 박준영 on 1/17/24.
+//
+
+import Foundation
+import CoreLocation
+
+// info.plist 에 권한 추가
+// Privacy - Location When In Use Usage Description
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    static let shared = LocationManager()
+    
+    let locationManager = CLLocationManager()
+    @Published var currentLocation: CLLocation?
+    @Published var isUpdatingLocation: Bool = false
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func getCurrentLocation() {
+        // 현재 위치를 일회성으로 전달
+        // https://developer.apple.com/documentation/corelocation/cllocationmanager/1620548-requestlocation
+        locationManager.requestLocation()
+        currentLocation = locationManager.location
+    }
+    
+    func startUpdatingLocation() {
+        // https://developer.apple.com/documentation/corelocation/cllocationmanager/1423750-startupdatinglocation
+        locationManager.startUpdatingLocation()
+        isUpdatingLocation = true
+    }
+    
+    func stopUpdatingLocation() {
+        // https://developer.apple.com/documentation/corelocation/cllocationmanager/1423695-stopupdatinglocation
+        isUpdatingLocation = false
+        locationManager.stopUpdatingLocation()
+    }
+    
+    // 지속적으로 위치 데이터 업데이트
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations.first
+    }
+    
+    // 오류 처리: 문제가 있으면 오류를 출력
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
