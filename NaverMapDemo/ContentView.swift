ef7d97f90ef3fe9fa4562c32b11198e7e28a1f5e
godlingto <godlingto@gmail.com>
2024-01-17 15:50:45 +0900

    마커(지도상의 한 지점을 나타내기 위한 오버레이)와 탭 했을 때 처리 추가

--- NaverMapDemo/ContentView.swift
+++ NaverMapDemo/ContentView.swift
@@ -10,7 +10,7 @@ import NMapsMap
 
 struct ContentView: View {
     
-    @State var coord: (Double, Double) = (126.974375, 37.579081)
+    @State var coord: MyCoord = MyCoord("경복궁 근처", 37.579081, 126.974375)
     
     var body: some View {
         VStack {
@@ -18,12 +18,10 @@ struct ContentView: View {
                 .ignoresSafeArea(.all, edges: .top)
             HStack {
                 Button("서울") {
-                    // 경복궁 근처
-                    coord = (126.974375, 37.579081)
+                    coord = MyCoord("경복궁 근처", 37.579081, 126.974375)
                 }
                 Button("부산") {
-                    // 해운대 근처
-                    coord = (129.153612, 35.157231)
+                    coord = MyCoord("해운대 근처", 35.157231, 129.153612)
                 }
             }
         }
@@ -33,7 +31,7 @@ struct ContentView: View {
 
 struct NaverMap: UIViewRepresentable {
     
-    var coord: (Double, Double)
+    var coord: MyCoord
     
     func makeCoordinator() -> Coordinator {
         Coordinator(coord)
@@ -56,7 +54,7 @@ struct NaverMap: UIViewRepresentable {
     func updateUIView(_ uiView: NMFNaverMapView, context: Context) {
         // NMGLatLng: 하나의 위경도 좌표를 나타내는 클래스
         // https://navermaps.github.io/ios-map-sdk/guide-ko/2-2.html
-        let coord = NMGLatLng(lat: coord.1, lng: coord.0)
+        let coord = NMGLatLng(lat: coord.lat, lng: coord.lng)
         
         // 카메라 이동
         // https://navermaps.github.io/ios-map-sdk/guide-ko/3-2.html
@@ -71,11 +69,25 @@ struct NaverMap: UIViewRepresentable {
         let locationOverlay = uiView.mapView.locationOverlay
         locationOverlay.hidden = false
         locationOverlay.location = coord
+
+        // 마커 : 지도상의 한 지점을 나타내기 위한 오버레이
+        // https://navermaps.github.io/ios-map-sdk/guide-ko/5-2.html
+        let marker = NMFMarker()
+        marker.position = coord
+        marker.captionText = self.coord.name
+        marker.mapView = uiView.mapView
+        // 마커 탭(클릭)했을 때 동작
+        marker.touchHandler = { (overlay: NMFOverlay) -> Bool in
+            print("Tap marker \(self.coord.name)")
+            return true
+        }
     }
     
     class Coordinator: NSObject, NMFMapViewCameraDelegate {
-        var coord: (Double, Double)
-        init(_ coord: (Double, Double)) {
+        
+        var coord: MyCoord
+        
+        init(_ coord: MyCoord) {
             self.coord = coord
         }
         
@@ -90,6 +102,18 @@ struct NaverMap: UIViewRepresentable {
         }
     }
     
+}
+
+struct MyCoord {
+    var name: String
+    var lat: Double
+    var lng: Double
+    
+    init(_ name: String, _ lat: Double, _ lng: Double) {
+        self.name = name
+        self.lat = lat
+        self.lng = lng
+    }
 }
 
 //#Preview {
