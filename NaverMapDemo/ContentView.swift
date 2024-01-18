f5d07651b4f1b0cd7f1401282695264ec99a35ab
godlingto <godlingto@gmail.com>
2024-01-18 14:02:02 +0900

    Coordinator 클래스의 updateMapView() 코드를 역할 별로 분리(리팩토링) - 메서드(함수) 주석 설명 추가

--- NaverMapDemo/ContentView.swift
+++ NaverMapDemo/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
                 .ignoresSafeArea(.all, edges: .top)
             HStack {
                 Button("이동경로 그리기") {
-                    var locations = [
+                    let locations = [
                         CLLocationCoordinate2D(latitude: 37.560828, longitude: 126.970839),
                         CLLocationCoordinate2D(latitude: 37.562156, longitude: 126.972947),
                         CLLocationCoordinate2D(latitude: 37.563822, longitude: 126.972842),
@@ -73,7 +73,14 @@ struct NaverMap: UIViewRepresentable {
     }
 }
 
-
+/**
+ NMFNaverMapView를 래핑한 코디네이터 클래스
+ 
+ 바인딩된 값의 변경에 따라 자동으로 처리할 때는 UIViewRepresentable 프로토콜을 따르는 NaverMap에 추가하여 구현
+ NaverMap 클래스의 updateUIView 메서드가 값의 변경에 따라 자동으로 호출
+ 
+ 직접 접근이 필요한 경우 Coordinator 클래스에 메서드를 추가하여 호출
+ */
 final class Coordinator: NSObject, ObservableObject, NMFMapViewCameraDelegate {
     static let shared = Coordinator()
     
@@ -101,22 +108,35 @@ final class Coordinator: NSObject, ObservableObject, NMFMapViewCameraDelegate {
         view
     }
     
+    /**
+     경로 오버레이를 설정
+     */
     func setPathOverly(coords: [MyCoord]) {
-        var points = coords.map { myCoord in
+        let points = coords.map { myCoord in
             NMGLatLng(lat: myCoord.lat, lng: myCoord.lng)
         }
         
         drawPathOverlay(points: points)
     }
     
+    /**
+     경로 오버레이를 설정
+     */
     func setPathOverly(coords: [CLLocationCoordinate2D]) {
-        var points = coords.map { loc in
+        let points = coords.map { loc in
             NMGLatLng(lat: loc.latitude, lng: loc.longitude)
         }
         
         drawPathOverlay(points: points)
     }
     
+    /**
+     경로를 그리는 메서드
+     
+     기존에 추가된 경로를 초기화하고 그림
+     
+     경로의 첫번째 위치로 카메라 이동
+     */
     private func drawPathOverlay(points: [NMGLatLng]) {
         // 경로를 새롭게 추가하기 위해 기존 내용을 삭제
         removeAllPathOverlays()
@@ -127,64 +147,92 @@ final class Coordinator: NSObject, ObservableObject, NMFMapViewCameraDelegate {
         
         let coord = points.first!
         
-        // 지도확대/축소 설정
-        view.showZoomControls = true
-        view.mapView.zoomLevel = 15
-        
         // 카메라 이동
-        let cameraUpdate = NMFCameraUpdate(scrollTo: coord)
-        cameraUpdate.animation = .fly
-        cameraUpdate.animationDuration = 2
-        view.mapView.moveCamera(cameraUpdate)
+        moveCamera(coord: coord, animation: .fly, duration: 2)
     }
     
-    
+    /**
+     지도 확대/축소 설정
+     */
+    func setZoomLevel(zoomLevel: Double, showZoomControls: Bool = false) {
+        view.showZoomControls = showZoomControls
+        view.mapView.zoomLevel = zoomLevel
+    }
+
     func updateMapView(coord: MyCoord) {
         self.coord = coord
-        // 마커와 정보 창을 새롭게 추가하기 위해 기존 내용을 삭제
-        removeAllMakers()
-        removeAllInfoWindows()
-        
+
         // NMGLatLng: 하나의 위경도 좌표를 나타내는 클래스
         // https://navermaps.github.io/ios-map-sdk/guide-ko/2-2.html
         let coord = NMGLatLng(lat: coord.lat, lng: coord.lng)
         
         // 카메라 이동
-        // https://navermaps.github.io/ios-map-sdk/guide-ko/3-2.html
-        let cameraUpdate = NMFCameraUpdate(scrollTo: coord)
-        cameraUpdate.animation = .fly
-        cameraUpdate.animationDuration = 2
-        view.mapView.moveCamera(cameraUpdate)
+        moveCamera(coord: coord, animation: .fly, duration: 2)
         
-        // 위치 오버레이 : 사용자의 현재 위치를 나타내는 데 특화된 오버레이
-        // https://navermaps.github.io/ios-map-sdk/guide-ko/5-1.html
-        // https://navermaps.github.io/ios-map-sdk/guide-ko/5-5.html
+        // 마커와 정보 창을 새롭게 추가하기 위해 기존 내용을 삭제
+        removeAllMakers()
+        removeAllInfoWindows()
+        
+        // 위치 오버레이
+        setLocationOverlay(coord: coord)
+
+        // 정보창과 연결된 마커를 추가
+        addMarkerAndInfoWindow(coord: coord,
+                               caption: self.coord.name,
+                               infoTitle: "정보 창 내용/마커를 탭하면 닫힘")
+    }
+    
+    /**
+     카메라 이동
+     /https://navermaps.github.io/ios-map-sdk/guide-ko/3-2.html
+     */
+    func moveCamera(coord: NMGLatLng, animation: NMFCameraUpdateAnimation = .none, duration: TimeInterval = 1) {
+        let cameraUpdate = NMFCameraUpdate(scrollTo: coord)
+        cameraUpdate.animation = animation
+        cameraUpdate.animationDuration = duration
+        view.mapView.moveCamera(cameraUpdate)
+    }
+    
+    /**
+     위치 오버레이 : 사용자의 현재 위치를 나타내는 데 특화된 오버레이
+     https://navermaps.github.io/ios-map-sdk/guide-ko/5-1.html
+     https://navermaps.github.io/ios-map-sdk/guide-ko/5-5.html
+     */
+    func setLocationOverlay(coord: NMGLatLng) {
         let locationOverlay = view.mapView.locationOverlay
         locationOverlay.hidden = false
         locationOverlay.location = coord
-
-        // 마커 : 지도상의 한 지점을 나타내기 위한 오버레이
-        // https://navermaps.github.io/ios-map-sdk/guide-ko/5-2.html
+    }
+    
+    /**
+     정보창과 연결된 마커를 추가 (마커를 탭하면 정보창이 열림)
+     
+     마커 : 지도상의 한 지점을 나타내기 위한 오버레이
+     
+     https://navermaps.github.io/ios-map-sdk/guide-ko/5-2.html
+     
+     정보 창 : 마커의 위 또는 지도의 특정 지점에 부가적인 정보를 나타내기 위한 오버레이
+     
+     https://navermaps.github.io/ios-map-sdk/guide-ko/5-3.html
+     */
+    func addMarkerAndInfoWindow(coord: NMGLatLng, caption: String = "", infoTitle: String = "") {
+        // 마커
         let marker = NMFMarker()
         marker.position = coord
-        marker.captionText = self.coord.name
+        marker.captionText = caption
         marker.mapView = view.mapView
         markers.append(marker)
         
-        
-        // 정보 창 : 마커의 위 또는 지도의 특정 지점에 부가적인 정보를 나타내기 위한 오버레이
-        // https://navermaps.github.io/ios-map-sdk/guide-ko/5-3.html
-        // 정보 창을 마커와 연결
+        // 정보 창
         let infoWindow = NMFInfoWindow()
         let dataSource = NMFInfoWindowDefaultTextSource.data()
-        dataSource.title = "정보 창 내용/마커를 탭하면 닫힘"
+        dataSource.title = infoTitle
         infoWindow.dataSource = dataSource
-        infoWindow.open(with: marker)
+//        infoWindow.open(with: marker)
         
-        // 마커 탭(클릭)했을 때 동작
+        // 정보 창을 마커와 연결
+        // 마커 탭(클릭)했을 때 동작(정보 창과 상호작용)
         marker.touchHandler = { (overlay: NMFOverlay) -> Bool in
-            print("Tap marker \(self.coord.name)")
-            // 정보 창과 상호작용
             if let marker = overlay as? NMFMarker {
                 if marker.infoWindow == nil {
                     // 현재 마커에 정보 창이 열려있지 않을 경우 엶
@@ -198,38 +246,55 @@ final class Coordinator: NSObject, ObservableObject, NMFMapViewCameraDelegate {
         }
     }
     
-    private func removeAllMakers() {
+    /**
+     지도상의 모든 마커를 삭제
+     */
+    func removeAllMakers() {
         markers.forEach { marker in
             marker.mapView = nil
         }
         markers.removeAll()
     }
     
-    private func removeAllInfoWindows() {
+    /**
+     지도상의 모든 정보창을 삭제
+     */
+    func removeAllInfoWindows() {
         infoWindows.forEach { infoWindow in
             infoWindow.mapView = nil
         }
         infoWindows.removeAll()
     }
     
-    private func removeAllPathOverlays() {
+    /**
+     지도상의 모든 경로를 삭제
+     */
+    func removeAllPathOverlays() {
         pathOverlays.forEach { pathOverlay in
             pathOverlay.mapView = nil
         }
         pathOverlays.removeAll()
     }
     
+    
+    /**
+     카메라 이동이 시작되기 전 호출
+     */
     func mapView(_ mapView: NMFMapView, cameraWillChangeByReason reason: Int, animated: Bool) {
-        // 카메라 이동이 시작되기 전 호출
         print("============== 카메라 변경전 - reason: \(reason)")
     }
     
+    /**
+     카메라의 위치가 변경되면 호출
+     */
     func mapView(_ mapView: NMFMapView, cameraIsChangingByReason reason: Int) {
-        // 카메라의 위치가 변경되면 호출
         print("카메라 변경후 - reason: \(reason)")
     }
 }
 
+/**
+ 사용자 정의 지도 위치
+ */
 struct MyCoord {
     var name: String
     var lat: Double
