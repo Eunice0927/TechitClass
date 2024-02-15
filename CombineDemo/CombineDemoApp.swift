2fd4f7493442a5e717d09da37644bf610134b998
godlingto <godlingto@gmail.com>
2024-02-15 13:14:23 +0900

    Initial Commit

---
+++ CombineDemo/CombineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineDemoApp.swift
+//  CombineDemo
+//
+//  Created by 박준영 on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CombineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
